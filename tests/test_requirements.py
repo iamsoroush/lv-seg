a884254442455958ad01f6602ad40bd8c8e90b08
soroush <soroush.moazed@gmail.com>
2021-11-30 18:14:23 +0330

    test requirements

---
+++ tests/test_requirements.py
@@ -0,0 +1,28 @@
+import pathlib
+
+
+def test_requirements():
+    """Tests for main requirements."""
+
+    packages = [
+        "tensorflow>=2.7.0",
+        "pandas",
+        "scikit-learn",
+        "scikit-image",
+        "scipy",
+        "matplotlib",
+        "mlflow",
+        "abstractions-aimedic",
+        "SimpleITK",
+        "pyyaml",
+        "albumentations",
+        "tqdm",
+        "pytest",
+    ]
+
+    project_dir = pathlib.Path(__file__).parent.parent
+    print(project_dir)
+
+    with open(project_dir.joinpath("requirements.txt"), "r") as f:
+        reqs = f.read().split()
+        assert all(item in reqs for item in packages)
